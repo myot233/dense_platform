9b7ca755f9a7175721370d94fe04c6ece978c009
myot233 <gsycl2004@gmail.com>
2024-07-27 11:19:30 +0800

    optimization login.vue

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,22 +1,46 @@
 <template>
-    <el-dialog v-model="centerDialogVisible" title="请登录" width="400"  align-center>
-        <el-form label-width="auto">
+    <el-dialog v-model="centerDialogVisible"  width="400"  align-center>
+        
+        <el-text size="Large">{{ title }}</el-text>
+        <el-form label-width="auto" v-if="isLogin">
             <el-form-item label="账号:">
                 <el-input v-model="loginform.account"></el-input>
             </el-form-item>
             <el-form-item label="密码:">
                 <el-input v-model="loginform.password" type="password" />
             </el-form-item>
-            <el-form-item style="width: 165px;" label="验证码:">
-                <el-input v-model="loginform.verify"></el-input>
+            <div style="display: flex;">
+                <el-text style="margin-right: 12px;">验证码:</el-text>
+                <el-input style="width: 120px; margin-right: 12px;"></el-input>
+                <el-image ></el-image>
+            </div>
+
+        </el-form>
+        <el-form label-width="auto" v-else>
+            <el-form-item label="账号:">
+                <el-input v-model="registerform.account"></el-input>
+            </el-form-item>
+            <el-form-item label="密码:">
+                <el-input v-model="registerform.password" type="password" />
+            </el-form-item>
+            <el-form-item label="重复密码:">
+                <el-input v-model="registerform.password" type="password" />
+            </el-form-item>
+            <el-form-item label="验证码:" style="display: flex;">
+
+                <el-input style="width: 120px; margin-right: 12px;"></el-input>
+                <el-image ></el-image>
             </el-form-item>
 
         </el-form>
+
         <template #footer>
             <div class="dialog-footer">
-                <el-button @click="centerDialogVisible = false">注册</el-button>
-                <el-button type="primary" @click="centerDialogVisible = false">登录</el-button>
+                <el-button @click="isLogin= false" v-if="isLogin">注册</el-button>
+                <el-button type="primary" @click="centerDialogVisible = false">{{ isLogin ? "登录" : "注册" }}</el-button>
             </div>
+
+            
         </template>
     </el-dialog>
 </template>
@@ -30,19 +54,36 @@ interface LoginForm {
 interface RegisterForm {
     account:string,
     password:string,
+    password_repeat:string,
     verify:string,
 }
 
-import { ref } from "vue"
+
+import { ref,computed, watch } from "vue"
 
 const centerDialogVisible = defineModel({ default: false })
+const isLogin = ref(true);
+
+watch(centerDialogVisible,(value)=>{
+    if(value){
+        isLogin.value = true;
+    }
+})
+const title = computed(()=>{
+    isLogin ? "请登录":"请注册" 
+})
 const loginform = ref<LoginForm>({
     account:"",
     password:"",
     verify:"",
 });
 
-const registerform = ref<RegisterForm>();
+const registerform = ref<RegisterForm>({
+    account:"",
+    password:"",
+    password_repeat:"",
+    verify:""
+});
 
 
 </script>
