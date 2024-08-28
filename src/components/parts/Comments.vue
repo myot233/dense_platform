fd3cad5c14ea6b92b321439613a4153592fd67fc
userName <userEmail>
2024-08-28 21:24:14 +0800

    [feature] 增加用于展示后续评论的组件

---
+++ src/components/parts/Comments.vue
@@ -0,0 +1,23 @@
+﻿<script setup lang="ts">
+  import Comments from "@/components/parts/Comments.vue";
+
+  type Comment = {
+      user:string,
+      content:string
+  }
+  const props = defineProps<{
+    comments:Comment[]
+  }>()
+  
+</script>
+
+<template>
+    <div v-for="comment in props.comments">
+      <el-avatar src="" ></el-avatar>
+      <el-text>{{comment.content}}</el-text>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
