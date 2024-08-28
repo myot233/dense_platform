a3cbbd3b264d9b67c0bf17b5d825cdfe9a8c9c58
userName <userEmail>
2024-08-28 21:25:50 +0800

    [fix] 去除了无用的注释

--- src/components/pages/History.vue
+++ src/components/pages/History.vue
@@ -8,11 +8,6 @@
         <el-breadcrumb separator="/">
         <el-breadcrumb-item >历史记录</el-breadcrumb-item>
         <el-breadcrumb-item v-if="  route.path.split('/').length >= 3 ">{{ path }}</el-breadcrumb-item>
-          <!-- <el-breadcrumb-item :to="{ path: './page-header.html' }">
-            homepage
-          </el-breadcrumb-item>
-          <el-breadcrumb-item><a href="./page-header.html">route 1</a></el-breadcrumb-item>
-          <el-breadcrumb-item>route 2</el-breadcrumb-item> -->
         </el-breadcrumb>
       </template>
     </el-page-header>
